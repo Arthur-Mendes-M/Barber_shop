17965a7aa4d3f179dea51ab22d708c7b9f464dce
Arthur mendes <arthurmendes@MacBook-Air-de-Arthur.local>
2024-03-28 21:26:11 -0300

    feat: search bar was added on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,25 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
 
-      <div className="p-5">
+      <section className="p-5">
         <h2 className="text-2xl">Olá, Arthur</h2>
         <p>{
           format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })
         }</p>
-      </div>
+      </section>
 
-      
-    </div>
+      <section className="px-5">
+        <Search />
+      </section>
+    </main>
   );
 }
